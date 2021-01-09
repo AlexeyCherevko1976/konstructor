c38086e01c00df3a5538b7b6d418346259e69561
Alexey Cherevko <you@example.com>
2021-01-09 09:48:54 +0700

    Z-21-2 POST-zaprosy

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express=require("express");
+const bodyParser=require("body-parser");
+
 const app=express();
+const urlencodedParser=bodyParser.urlencoded({extended: false});
+
+
+
 app.use("/static", express.static(__dirname+"/public"));
+
+app.get("/register", urlencodedParser, function(request, response){
+	response.sendFile(__dirname+"/public/register.html");
+})
+app.post("/register", urlencodedParser, function(request, response){
+	if(!request.body) return response.sendStatus(400);
+	console.log(request.body);
+	response.send(`${request.body.userName}-${request.body.userAge}`)
+})
 app.get("/", function(request, response){
 	response.send("<h2> Main Page</h2>")
 })
